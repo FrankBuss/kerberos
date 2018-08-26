0c059023efba9c482d0fff1c4fa7c2e3adae9142
Frank Buss <fb@frank-buss.de>
2018-08-26 14:45:32 +0200

    new build script for Qt 5.8 on Windows

--- qt/build-windows.cmd
+++ qt/build-windows.cmd
@@ -1,26 +1,34 @@
-pushd %CD%
-call "C:\Qt\Qt5.3.2\5.3\mingw482_32\bin\qtenv2.bat"
-popd
+rem
+rem this script mus be started from a "Qt 5.8 for Desktop (MinGW 5.3.0 32 bit)" shell
+rem
 
-del makefile
-del makefile.*
-qmake.exe kerberos.pro -r -spec win32-g++ "CONFIG+=release"
+rem clean
+rem
+rmdir /q /s debug
+rmdir /q /s release
+del /q *.bak
+del /q Makefile
+del /q Makefile.*
+del /q *.pdb
+del /q ui_*.*
+
+rem
+rem compile application
+rem
+qmake.exe kerberos.pro
 mingw32-make clean
 mingw32-make
 
+rem
+rem copy all files to the export directory
+rem
 rmdir /s /q kerberos
 mkdir kerberos
-mkdir kerberos\imageformats
-mkdir kerberos\platforms
-copy release\kerberos.exe kerberos
-copy C:\Qt\Qt5.3.2\5.3\mingw482_32\bin\Qt5Core.dll kerberos
-copy C:\Qt\Qt5.3.2\5.3\mingw482_32\bin\Qt5Gui.dll kerberos
-copy C:\Qt\Qt5.3.2\5.3\mingw482_32\bin\Qt5Widgets.dll kerberos
-copy C:\Qt\Qt5.3.2\5.3\mingw482_32\bin\libgcc_s_dw2-1.dll kerberos
-copy "C:\Qt\Qt5.3.2\Tools\mingw482_32\bin\libstdc++-6.dll" kerberos
-copy C:\Qt\Qt5.3.2\Tools\mingw482_32\bin\libwinpthread-1.dll kerberos
-copy C:\Qt\Qt5.3.2\5.3\mingw482_32\bin\icu*.dll kerberos
-copy C:\Qt\Qt5.3.2\5.3\mingw482_32\plugins\imageformats\qico.dll kerberos\imageformats
-copy C:\Qt\Qt5.3.2\5.3\mingw482_32\plugins\imageformats\qjpeg.dll kerberos\imageformats
-copy C:\Qt\Qt5.3.2\5.3\mingw482_32\plugins\imageformats\qmng.dll kerberos\imageformats
-copy C:\Qt\Qt5.3.2\5.3\mingw482_32\plugins\platforms\qwindows.dll kerberos\platforms
+xcopy /y release\kerberos.exe kerberos
+xcopy /y flux-cuda.dll kerberos
+xcopy /y default.ini kerberos
+windeployqt -printsupport kerberos\kerberos.exe
+xcopy /y mingw32_dll\libquadmath-0.dll kerberos
+xcopy /y mingw32_dll\libgcc_s_sjlj-1.dll kerberos
+xcopy /y mingw32_dll\libgfortran-3.dll kerberos
+xcopy /y %QWT_ROOT%\lib\qwt.dll kerberos
