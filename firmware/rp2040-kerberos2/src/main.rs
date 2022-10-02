7ca929c2218e0a506b325dee255c2f99d19760cb
Frank Buss <fb@frank-buss.de>
2022-10-02 20:21:41 +0200

    copied code from https://github.com/rp-rs/sample-project

--- firmware/rp2040-kerberos2/src/main.rs
+++ firmware/rp2040-kerberos2/src/main.rs
@@ -1,84 +1,117 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
+use core::sync::atomic::{AtomicUsize, Ordering};
+use cortex_m_rt::entry;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
-use hal::pac;
+use pac::{watchdog, xosc};
 use panic_probe as _;
-use rp2040_hal as hal;
-use rp2040_hal::clocks::init_clocks_and_plls;
-use rp2040_hal::Clock;
-use rp2040_hal::{gpio::Pins, watchdog::Watchdog, Sio};
+use rp2040_pac as pac;
 
-#[cfg(feature = "rt")]
-extern crate cortex_m_rt;
-#[cfg(feature = "rt")]
-pub use hal::entry;
+mod pll;
+mod resets;
 
-/// The linker will place this boot block at the start of our program image. We
-/// need this to help the ROM bootloader get our code up and running.
-#[cfg(feature = "boot2")]
 #[link_section = ".boot2"]
-#[no_mangle]
 #[used]
 pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
-#[hal::entry]
-fn main() -> ! {
-    info!("Program start");
-
-    let mut pac = pac::Peripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
-    let clocks = init_clocks_and_plls(
-        XOSC_CRYSTAL_FREQ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    let sio = Sio::new(pac.SIO);
-    let pins = Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-    // Set a pin to drive output
-    let mut led_pin = pins.gpio25.into_push_pull_output();
-    /*
-        // Drive output to 3.3V
-        output_pin.set_high().unwrap();
-        // Drive output to 0V
-        output_pin.set_low().unwrap();
-        // Set a pin to input
-        let input_pin = pins.gpio24.into_floating_input();
-        // pinstate will be true if the pin is above 2V
-        let pinstate = input_pin.is_high().unwrap();
-        // pinstate_low will be true if the pin is below 1.15V
-        let pinstate_low = input_pin.is_low().unwrap();
-        // you'll want to pull-up or pull-down a switch if it's not done externally
-        let button_pin = pins.gpio23.into_pull_down_input();
-        let button2_pin = pins.gpio22.into_pull_up_input();
-    */
-
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-    loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(100);
-        led_pin.set_low().unwrap();
-        delay.delay_ms(100);
-    }
+#[defmt::timestamp]
+fn timestamp() -> u64 {
+    static COUNT: AtomicUsize = AtomicUsize::new(0);
+    // NOTE(no-CAS) `timestamps` runs with interrupts disabled
+    let n = COUNT.load(Ordering::Relaxed);
+    COUNT.store(n + 1, Ordering::Relaxed);
+    n as u64
 }
 
-// End of file
+fn init(
+    resets: pac::RESETS,
+    watchdog: pac::WATCHDOG,
+    clocks: pac::CLOCKS,
+    xosc: pac::XOSC,
+    pll_sys: pac::PLL_SYS,
+    pll_usb: pac::PLL_USB,
+) {
+    // Now reset all the peripherals, except QSPI and XIP (we're using those
+    // to execute from external flash!)
+
+    let resets = resets::Resets::new(resets);
+
+    // Reset everything except:
+    // - QSPI (we're using it to run this code!)
+    // - PLLs (it may be suicide if that's what's clocking us)
+    resets.reset(!(resets::IO_QSPI | resets::PADS_QSPI | resets::PLL_SYS | resets::PLL_USB));
+
+    resets.unreset_wait(
+        resets::ALL
+            & !(resets::ADC
+                | resets::RTC
+                | resets::SPI0
+                | resets::SPI1
+                | resets::UART0
+                | resets::UART1
+                | resets::USBCTRL),
+    );
+
+    // xosc 12 mhz
+    watchdog
+        .tick
+        .write(|w| unsafe { w.cycles().bits(XOSC_MHZ as u16).enable().set_bit() });
+
+    clocks.clk_sys_resus_ctrl.write(|w| unsafe { w.bits(0) });
+
+    // Enable XOSC
+    // TODO extract to HAL module
+    const XOSC_MHZ: u32 = 12;
+    xosc.ctrl.write(|w| w.freq_range()._1_15mhz());
+    let startup_delay = (((XOSC_MHZ * 1_000_000) / 1000) + 128) / 256;
+    xosc.startup
+        .write(|w| unsafe { w.delay().bits(startup_delay as u16) });
+    xosc.ctrl
+        .write(|w| w.freq_range()._1_15mhz().enable().enable());
+    while !xosc.status.read().stable().bit_is_set() {}
+
+    // Before we touch PLLs, switch sys and ref cleanly away from their aux sources.
+    clocks.clk_sys_ctrl.modify(|_, w| w.src().clk_ref());
+    while clocks.clk_sys_selected.read().bits() != 1 {}
+    clocks.clk_ref_ctrl.modify(|_, w| w.src().rosc_clksrc_ph());
+    while clocks.clk_ref_selected.read().bits() != 1 {}
+
+    resets.reset(resets::PLL_SYS | resets::PLL_USB);
+    resets.unreset_wait(resets::PLL_SYS | resets::PLL_USB);
+
+    pll::PLL::new(pll_sys).configure(1, 1500_000_000, 6, 2);
+    pll::PLL::new(pll_usb).configure(1, 480_000_000, 5, 2);
+}
+
+#[entry]
+fn main() -> ! {
+    info!("Hello World!");
+
+    let p = pac::Peripherals::take().unwrap();
+
+    init(p.RESETS, p.WATCHDOG, p.CLOCKS, p.XOSC, p.PLL_SYS, p.PLL_USB);
+
+    let led_pin = 25;
+
+    loop {
+        info!("on!");
+        p.IO_BANK0.gpio[led_pin].gpio_ctrl.write(|w| {
+            w.oeover().enable();
+            w.outover().high();
+            w
+        });
+
+        cortex_m::asm::delay(1_000_000);
+
+        info!("off!");
+        p.IO_BANK0.gpio[led_pin].gpio_ctrl.write(|w| {
+            w.oeover().enable();
+            w.outover().low();
+            w
+        });
+
+        cortex_m::asm::delay(1_000_000);
+    }
+}
