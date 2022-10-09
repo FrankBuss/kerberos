ac35b64fab57c2c510d234d294116d75dc3a4028
Frank Buss <fb@frank-buss.de>
2022-10-09 05:20:09 +0200

    first version of the Zephyr firmware for the RP2040

---
+++ firmware/rp2040-kerberos2/src/main.c
@@ -0,0 +1,27 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define SLEEP_TIME_MS 500
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+void main(void) {
+	int ret;
+
+	if (!device_is_ready(led.port)) {
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return;
+		}
+		k_msleep(SLEEP_TIME_MS);
+	}
+}
